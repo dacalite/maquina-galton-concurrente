a3ffd95c3ec2eeea48b5c75b6fe0b2b1e60c450b
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-09 13:43:30 +0200

    basic springboot initializr template

---
+++ src/main/java/com/concu_augusto_sergio/maquinagalton/MaquinagaltonApplication.java
@@ -0,0 +1,13 @@
+package com.concu_augusto_sergio.maquinagalton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaquinagaltonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaquinagaltonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concu_augusto_sergio/maquinagalton/MaquinagaltonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concu_augusto_sergio.maquinagalton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaquinagaltonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
