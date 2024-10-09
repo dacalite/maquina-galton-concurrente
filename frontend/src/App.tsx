8a32cd0ca9392815a4b8238c04c71919d2ee6d82
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-10-10 00:35:15 +0200

    fix: app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
+import ConfiguracionFabrica from './components/ConfiguracionFabrica'
 import TableroGalton from './components/TableroGalton'
 
 function App() {
   return (
-    <div className='grid grid-cols-3 grid-rows-1 gap-4 w-screen h-screen p-0 m-0'>
-      <div className='col-span-2 w-full h-full'>Backend Animation</div>
-      <div className='col-start-3 w-full h-full flex justify-center items-center'>
-        <TableroGalton levels={8} />
+    <div className='grid grid-cols-6 grid-rows-1 gap-4 w-screen h-screen p-0 m-0'>
+      <div className='col-span-4 w-full h-full p-12 flex justify-center items-center'>
+        <ConfiguracionFabrica />
+      </div>
+      <div className='col-start-5 col-span-2 w-full h-full flex justify-center items-center'>
+        <TableroGalton levels={8} numDividers={11} />
       </div>
     </div>
   )
